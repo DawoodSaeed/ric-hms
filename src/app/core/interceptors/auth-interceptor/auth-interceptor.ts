fa3c5e09d90d339375df04504cbe0bdf6085a1ba
muhammaddawoodsaeed <dawoodsaeed.work@gmail.com>
2025-02-16 14:54:16 +0500

    -=-

---
+++ src/app/core/interceptors/auth-interceptor/auth-interceptor.ts
@@ -0,0 +1,20 @@
+import { inject, Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return of();
+  }
+}
